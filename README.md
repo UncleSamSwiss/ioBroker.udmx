3afcfb117fdf3c5b22c1366a1be7c32cb06c9ac0
Samuel Weibel <samuel.weibel@gmail.com>
2018-03-13 19:42:04 +0100

    Initial version

---
+++ README.md
@@ -0,0 +1,60 @@
+![uDMX Logo](admin/udmx.png)
+
+# ioBroker adapter for uDMX
+
+[![NPM version](http://img.shields.io/npm/v/iobroker.udmx.svg)](https://www.npmjs.com/package/iobroker.udmx) [![Downloads](https://img.shields.io/npm/dm/iobroker.udmx.svg)](https://www.npmjs.com/package/iobroker.udmx) [![Travis](https://img.shields.io/travis/UncleSamSwiss/ioBroker.udmx.svg)](https://travis-ci.org/UncleSamSwiss/ioBroker.udmx/) [![GitHub issues](https://img.shields.io/github/issues/UncleSamSwiss/ioBroker.udmx.svg)](https://github.com/UncleSamSwiss/ioBroker.udmx/issues)
+
+Talks to DMX devices using the Anyma uDMX USB adapter.
+
+## Requirements
+
+You need to have an Anyma uDMX compatible USB adapter. Buy one at one of the following sources:
+
+* [Official Anyma Store](http://www.anyma.ch/store/11-udmx.html)
+* [Clones found on Aliexpress](https://www.aliexpress.com/wholesale?SearchText=USB+dmx)
+
+*IMPORTANT* The author of this adapter is not related to any of the above companies and does not take any responsibility for the proper working of the hardware bought at those links!
+
+## Install
+
+Before installing, please read the [installation guide of the USB module](https://www.npmjs.com/package/usb#installation).
+
+Especially make sure, that you have libudev installed:
+
+	sudo apt-get install build-essential libudev-dev
+
+## Configuration
+
+### Number of addresses
+
+This is the number of DMX addresses that should be available as states in ioBroker.
+
+Addresses will always start at 1 and go through to the configured number of addresses.
+
+If you have for example a [12 channel DMX decoder for RGB LEDs](https://www.aliexpress.com/item/12CH-DMX-Decoder-Inpot-DC5V-24V-LED-DMX512-controller-high-power-RGB-Controller-Factory-Outlet/920810599.html)
+configured with and address offset of 0 (all dip switches to OFF), you would need 12 addresses and they will be numbered in ioBroker as 1 through 12.
+
+A maximum of 512 addresses is possible.
+
+## Compatibility
+
+Compatibility has been tested with Raspberry Pi 3.
+
+## Bug Reports and Feature Requests
+
+Please use the GitHub repository to report any bugs or request new features.
+
+If you require a missing devcies, please provide the type of IC (brand, model, ...) and its address(es) as reported in the adapter configuration.
+
+## TODO
+* Add possibility to combine states into a channel (e.g. "RGB", "RGBW" or "warm white/cold white")
+
+## Changelog
+
+### 0.0.1
+* (UncleSamSwiss) Initial version
+
+## Thanks
+
+This project uses the [usb](https://www.npmjs.com/package/usb) NPM module and was inspired by [homebridge-udmx](https://github.com/Piets/homebridge-udmx/blob/master/index.js).
+Thanks to the authors of those packages for their excellent work!
---
+++ udmx.js
@@ -0,0 +1,155 @@
+﻿/* jshint -W097 */ // no "use strict" warnings
+/* jshint -W061 */ // no "eval" warnings
+/* jslint node: true */
+"use strict";
+
+// always required: utils
+var utils = require(__dirname + '/lib/utils');
+
+// other dependencies:
+var usb = require('usb');
+var buffer = require('buffer');
+
+// These variables specify the anyma uDMX device
+var vendorId = 0x16c0;
+var productId = 0x5dc;
+
+function DmxAdapter() {
+    var that = this;
+    
+    // public fields
+    that.adapter = utils.adapter('udmx'); // create the adapter object
+    
+    // private fields
+    that._dmxBuffer = null;
+    that._udmxDevice = null;
+    
+    // register event handlers
+    that.adapter.on('ready', function () {
+        that.onReady();
+    });
+    that.adapter.on('stateChange', function (id, state) {
+        that.onStateChange(id, state);
+    });
+    that.adapter.on('unload', function (callback) {
+        that.onUnload(callback);
+    });
+}
+
+
+DmxAdapter.prototype.main = function (allStates) {
+    var that = this;
+    
+    var addressCount = parseInt(that.adapter.config.addressCount);
+    if (!addressCount) {
+        that.adapter.log.warn("No addresses configured, nothing to do in this adapter");
+        return;
+    }
+    
+    that._dmxBuffer = Buffer.alloc(addressCount);
+    
+    if (that.adapter.config.mockDevice) {
+        that.adapter.log.warn("Using a mock USB device for testing!");
+        that._udmxDevice = new MockUsbDevice(that.adapter.log);
+    }
+    else {
+        that._udmxDevice = usb.findByIds();
+        if (!that._udmxDevice) {
+            that.adapter.log.error("Couldn't find an Anyma USB adapter, not doing anything!");
+            return;
+        }
+        
+        that._udmxDevice.open();
+    }
+    
+    for (var i = 1; i <= addressCount; i++) {
+        var id = '' + i;
+        var obj = {
+            type: 'state',
+            common: {
+                name: 'Address ' + i,
+                read: true,
+                write: true,
+                type: 'number',
+                role: 'level',
+                min: 0,
+                max: 255
+            },
+            native: {}
+        };
+        that.adapter.setObject(id, obj);
+        var existingValue = allStates[that.adapter.namespace + '.' + i];
+        if (existingValue) {
+            that._dmxBuffer.writeUInt8(parseInt(existingValue.val), i - 1);
+        }
+        else {
+            that.setStateAck(id, 0);
+        }
+    }
+    
+    that.adapter.subscribeStates('*');
+    that.sendDMXBuffer();
+};
+
+DmxAdapter.prototype.setStateAck = function (id, value) {
+    this.adapter.setState(id, {val: value, ack: true});
+};
+
+// startup
+DmxAdapter.prototype.onReady = function () {
+    var that = this;
+    that.adapter.getStates('*', function (err, states) {
+        that.main(states);
+    });
+};
+
+// is called if a subscribed state changes
+DmxAdapter.prototype.onStateChange = function (id, state) {
+    // Warning: state can be null if it was deleted!
+    if (!id || !state || state.ack) {
+        return;
+    }
+    
+    var that = this;
+    that.adapter.log.debug('stateChange ' + id + ' ' + JSON.stringify(state));
+    var index = parseInt(id.replace(that.adapter.namespace + '.', '')) - 1;
+    that._dmxBuffer.writeUInt8(state.val, index);
+    that.sendDMXBuffer();
+
+    that.setStateAck(id, state.val);
+};
+
+// unloading
+DmxAdapter.prototype.onUnload = function (callback) {
+    var that = this;
+    if (that._udmxDevice) {
+        that._udmxDevice.close();
+    }
+    
+    callback();
+};
+
+// send the DMX buffer to the USB device
+DmxAdapter.prototype.sendDMXBuffer = function () {
+    var that = this;
+    if (that._udmxDevice) {
+        that._udmxDevice.controlTransfer(0x40, 0x0002, that._dmxBuffer.length, 0, that._dmxBuffer, function (error) {
+            that.adapter.log.error('USB error: ' + error);
+        });
+    }
+};
+
+// Mock USB device for testing on Windows
+function MockUsbDevice(logger) {
+    this.logger = logger;
+}
+
+MockUsbDevice.prototype.close = function () {
+    this.logger.info('[Mock USB] Closing');
+};
+
+MockUsbDevice.prototype.controlTransfer = function (requestType, request, length, offset, buffer, errorCallback) {
+    this.logger.info('[Mock USB] Transferring ' + buffer.toString('hex'));
+};
+
+new DmxAdapter();
